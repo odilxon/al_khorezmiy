47c6bc62334f664c6f2bcc75ce6bbac1ecc12904
Odilxon <mmuhtor@gmail.com>
2021-02-11 19:12:08 +0500

    html added

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def main():
-    return "Al Khorezmiy"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
