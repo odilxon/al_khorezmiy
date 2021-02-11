d323a0736d3789ed2d5f97b3cd3b0a2ec4d1ac5e
Odilxon <mmuhtor@gmail.com>
2021-02-11 20:25:55 +0500

    travis ci

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route('/test')
+def test():
+    return "Works!"
+
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
