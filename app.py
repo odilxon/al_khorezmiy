62df055fc62a7adf5afb5d718e4d681b60960a83
Jurody <kamilov.2000@gmail.com>
2021-02-13 14:42:00 +0500

    try

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -7,9 +7,16 @@ db = SQLAlchemy(app)
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-@app.route('/')
+
+@app.route('/', methods = ["GET"])
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
