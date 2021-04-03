7fbcdfd27bff71a32771bb323037bbc9ea976ca8
AAbdurakhmanov <aabdurakhmanov98@gmail.com>
2021-04-04 00:22:16 +0500

    not easy

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegisterationForm, LoginForm, SubmitYourArticleForm
-from datetime import datetime
-from flask_login import LoginManager
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///data.db'
-db = SQLAlchemy(app)
-
-app.config['SECRET_KEY'] = '4079d33f50e34921722161947c3cab26'
+from forms import *
 
 posts = [
     {
@@ -25,20 +15,64 @@ posts = [
     }
 ]
 
-@app.route('/', methods = ["GET"])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        print("vali")
+        user = User.query.filter_by(login=form.username.data).first()
+        if user is None or not user.password == form.password.data:
+            
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    print(form.errors)
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print('val')
+        user = User(login=form.username.data, email=form.email.data,name=form.username.data,role='admin', phone=form.phone.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        print('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    print("dd")
+    return render_template('register.html', title='Register', form=form)
+
+@app.context_processor
+def utility_processor():
+    def Capi(name):
+        return name.split(".")[0][0].upper() + name.split(".")[0][1::] #returning name of the file name wo extension and w first letter uppercase
+    return dict(Capi=Capi)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
-    
+    return render_template("about.html")
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html')
-
+    return render_template("articles.html")
 
 @app.route('/articlelist')
 def articlelist():
@@ -56,6 +90,7 @@ def issueyears():
 
 
 @app.route('/submitarticle')
+@login_required
 def submitarticle():
     return render_template('submitarticle.html')
 
@@ -65,29 +100,11 @@ def foraccount():
 
 
 
-@app.route('/register', methods=['GET','POST'])
-def register():
-    form = RegisterationForm()
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route('/login', methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Login', form=form)
-    if request.method == 'POST':
-        if  login(request.form['username'],
-                  request.form['password']):
-            return user(request.form['blabla'])
-        else:
-            error = 'Invalid username/password'
-    return render_template('foraccount.html', error=error)
-
 @app.route("/tests")
 def test():
     return "Works!"
     
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
 
